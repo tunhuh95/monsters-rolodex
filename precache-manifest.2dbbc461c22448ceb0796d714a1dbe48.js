d09f93633dc0ab65455c2d26e0209efb6b2cd837
Tuan Huynh <vantuan.huynh@foodycorp.vn>
2020-03-29 20:33:39 +0700

    Updates

---
+++ precache-manifest.2dbbc461c22448ceb0796d714a1dbe48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ebb5d5f573122c44813b4288d35dad",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "59ee99ad42c569dae1c6",
+    "url": "/monster-rolodex/static/css/main.cad13da4.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ee99ad42c569dae1c6",
+    "url": "/monster-rolodex/static/js/main.1d840948.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
